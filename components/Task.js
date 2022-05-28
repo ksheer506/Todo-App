349795de197ae90e814b63f8e3dcd3140d64803f
ksheer506 <ksheer506@naver.com>
2022-05-28 22:46:37 +0900

    React: 완료, 미완료 할일 목록 이동

--- components/Task.js
+++ components/Task.js
@@ -1,3 +1,5 @@
+/* const { useState, useEffect, useRef, useCallback } = React; */
+
 const taskObj1 = [
   { title: "할일1", id: "id_4577435", dueDate: "2022-05-25", text: "텍스트1", isCompleted: false },
   { title: "할일2", id: "id_4545535", dueDate: "2022-04-25", text: "텍스트2", isCompleted: false },
@@ -9,23 +11,26 @@ const taskObj2 = [
   { title: "할일7", id: "id_4177435", text: "텍스트3", isCompleted: true },
 ];
 
-function DatePicker(props) {
-  const { id, dueDate, onChange } = props
-
+const MemoDatePicker = React.memo(function DatePicker({ id, dueDate, onChange }) {
   return (
     <label className="dueDate" htmlFor={`datepicker ${id}`}>
       <input type="date" id={`datepicker ${id}`} onChange={onChange}></input>
       {dueDate}
     </label>
   );
-}
+});
 
 function Task(props) {
   const [title, setTitle] = React.useState(props.title);
   const [dueDate, setDueDate] = React.useState(props.dueDate);
+
   const onChangeDueDate = (e) => {
     setDueDate(e.target.value);
   };
+
+
+  const chkBox = useRef(null);
+
   const propsDueDate = {
     id: props.id,
     onChange: onChangeDueDate,
@@ -35,23 +40,26 @@ function Task(props) {
   return (
     <li className="task" id={props.id}>
       <div className="task-label">
-        <input type="checkbox"></input>
+        <input
+          type="checkbox"
+          onChange={() => props.onChangeCompletion(props)}
+          checked={props.isCompleted}
+          ref={chkBox} />
         {title}
       </div>
       <div className="task-tags"></div>
       <div className="extra">
-        <DatePicker {...propsDueDate} />
+        <MemoDatePicker {...propsDueDate} />
         <div className="close" tabIndex="0">x</div>
       </div>
     </li>
   );
 };
 
-function TaskListSection(props) {
-  const { sectionClass, taskArr } = props;
+function TaskListSection({ sectionClass, taskArr, onChangeCompletion }) {
   const sectionName = (sectionClass === "ongoing") ? "진행중" : "완료";
   const tasks = taskArr.map((obj) => {
-    return <Task {...obj} key={obj.id} />
+    return <Task {...obj} onChangeCompletion={onChangeCompletion} key={obj.id} />
   });
 
   return (
@@ -66,14 +74,53 @@ function TaskListSection(props) {
   );
 }
 
-function TaskList(ongoing, completed) {
+function TaskList({ ongoing, completed }) {
+  const [ongoingArr, setOngoingArr] = useState(ongoing);
+  const [completedArr, setCompletedArr] = useState(completed);
+
+  const onChangeCompletion = (taskProps) => {
+    const { isCompleted, id } = taskProps;
+    const modifiedTaskProps = { ...taskProps, isCompleted: !taskProps.isCompleted };
+
+    if (!isCompleted) {
+      setOngoingArr(prevOngoing => {
+        return prevOngoing.filter(taskObj => taskObj.id !== id);
+      });
+      setCompletedArr(prevCompleted => {
+        const refreshedCompleted = prevCompleted.slice();
+        refreshedCompleted.push(modifiedTaskProps);
+        return refreshedCompleted;
+      });
+
+      return
+    }
+
+    setCompletedArr(prevCompleted => {
+      return prevCompleted.filter(taskObj => taskObj.id !== id);
+    });
+    setOngoingArr(prevOngoing => {
+      const refreshedOngoing = prevOngoing.slice();
+      refreshedOngoing.push(modifiedTaskProps);
+      return refreshedOngoing;
+    });
+
+  };
+
+  useEffect(() => {
+    /* console.log("미완료: ", ongoingArr);
+    console.log("완료: ", completedArr); */
+  }, [ongoingArr, completedArr]);
+
+
   const ongoingSect = {
     sectionClass: "ongoing",
-    taskArr: ongoing
+    taskArr: ongoingArr,
+    onChangeCompletion: onChangeCompletion
   };
   const completedSect = {
     sectionClass: "completed",
-    taskArr: completed
+    taskArr: completedArr,
+    onChangeCompletion: onChangeCompletion
   };
 
   return (
@@ -83,3 +130,5 @@ function TaskList(ongoing, completed) {
     </article>
   );
 }
+
+ReactDOM.render(<TaskList ongoing={taskObj1} completed={taskObj2} />, document.getElementById("root"));
