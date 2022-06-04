a0f136880824daadd189b1ef328adbb6f6bb3b61
ksheer506 <ksheer506@naver.com>
2022-06-04 23:01:20 +0900

    dummy data 삭제 및 indexedDB 이용

--- src/index.js
+++ src/index.js
@@ -3,19 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const taskObj1 = [
-  { title: "할일1", id: "id_1", dueDate: "2022-05-25", text: "텍스트1", isCompleted: false },
-  { title: "할일2", id: "id_2", dueDate: "2022-04-25", text: "텍스트2", isCompleted: false },
-  { title: "할일3", id: "id_3", text: "텍스트3", isCompleted: false },
-  { title: "할일5", id: "id_5", dueDate: "2022-05-25", text: "텍스트1", isCompleted: true },
-  { title: "할일6", id: "id_6", dueDate: "2022-04-25", text: "텍스트2", isCompleted: true },
-  { title: "할일7", id: "id_7", text: "텍스트3", isCompleted: true },
-];
+function rootRender(tasks) {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <React.StrictMode>
-    <App tasks={taskObj1} />
-  </React.StrictMode>
-);
+  root.render(
+    <React.StrictMode>
+      <App tasks={tasks} />
+    </React.StrictMode>
+  );
+};
+
+export default rootRender;
+
 
