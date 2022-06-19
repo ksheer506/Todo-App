f76314a40735cfb2f8764dbc530b26d49a7d80b1
ksheer506 <ksheer506@naver.com>
2022-06-19 21:22:48 +0900

    현재 작업(currentWork)으로 State 통합

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useEffect, useCallback, useMemo, useState } from 'react';
-import { accessTaskDB, accessTagDB } from './modules/db/access.js'
+import { accessTaskDB, accessTagDB } from './modules/db/access.js';
+import { FilterByTagsDB } from './modules/db/fetching.js'
 
 import { TaskListSection, Task } from "./components/Task.js";
 import { AddNewTask, AddNewTags } from "./components/AddNewItems.js"
 import SideMenu from "./components/SideMenu.js"
-import Tag from "./components/Tag.js"
+import { Tag, TagList } from "./components/Tag.js"
 
 class Todo {
   constructor(object) {
@@ -22,15 +23,15 @@ function findObj(data, id) {
   return data.filter(taskObj => taskObj.id === id)[0];
 };
 
-function mappingComponent(arr, Comp, extraProps) {
-  return arr.map((props) => (<Comp {...props}{...extraProps} key={props.id} />))
+function mappingComponent(arr, Component, extraProps) {
+  return arr.map((props) => (<Component {...props}{...extraProps} key={props.id} />))
 }
 
 function App({ tasks, tagList }) {
-  const [currentTask, setCurrentTask] = useState({ action: null, task: {} });
-  const [newTag, setNewTag] = useState("");
+  const [currentWork, setCurrentWork] = useState({ action: null, task: {}, tag: {} });
   const [side, setSide] = useState(null);
   const [tags, setTags] = useState(tagList);
+  const [selectedTags, setSelectedTags] = useState([]);
   const [taskArr, setTaskArr] = useState(tasks); // TODO:
 
   /* 할일 사이드 메뉴에서 보기 */
@@ -47,17 +48,16 @@ function App({ tasks, tagList }) {
 
     const newTaskInst = new Todo(task);
 
-    setCurrentTask({ action: "ADD", task: newTaskInst });
+    setCurrentWork({ action: "Task/ADD", task: newTaskInst });
     setTaskArr(prev => [...prev, newTaskInst]);
   }, []);
 
   /* 2. 할일 완료/미완료 여부 변경 */
   const toggleCompletion = useCallback((taskId) => {
     const taskObj = findObj(taskArr, taskId);
-    console.log(taskObj);
     const editedTask = { ...taskObj, isCompleted: !taskObj.isCompleted };
 
-    setCurrentTask({ action: "MODIFY", task: editedTask }); // 어따 쓰나?
+    setCurrentWork({ action: "Task/MODIFY", task: editedTask }); // 어따 쓰나?
     setTaskArr(prev => {
       return prev.map((task) => {
         if (task.id === taskId) {
@@ -72,7 +72,7 @@ function App({ tasks, tagList }) {
   const deleteTask = useCallback((taskId) => {
     const taskObj = findObj(taskArr, taskId);
 
-    setCurrentTask({ action: "DELETE", task: taskObj });
+    setCurrentWork({ action: "Task/DELETE", task: taskObj });
     setTaskArr(prev => {
       return prev.filter(task => task.id !== taskId);
     });
@@ -83,7 +83,7 @@ function App({ tasks, tagList }) {
     const taskObj = findObj(taskArr, taskId);
     const editedTask = { ...taskObj, dueDate: e.target.value };
 
-    setCurrentTask({ action: "MODIFY", task: editedTask });
+    setCurrentWork({ action: "Task/MODIFY", task: editedTask });
     setTaskArr(prev => {
       return prev.map((task) => {
         if (task.id === taskId) {
@@ -94,34 +94,36 @@ function App({ tasks, tagList }) {
     });
   }, [taskArr]);
 
-  /* Task DB 업데이트 */
+  /* DB 업데이트 */
   useEffect(() => {
-    const { action, task } = currentTask;
+    const { action, task, tag } = currentWork;
     if (!action) return;
 
-    accessTaskDB(action, task);
-  }, [currentTask])
+    const [db, operation] = action.match(/\w{1,10}/g);
 
-  const addTagCallbacks = useCallback({
-    newTagName: (e) => {
-      setNewTag(e.target.value);
-    },
-    addTag: () => {
-      // [{tagText: "태그1", id: "태그1", assignedTask: []}, { ... }, ...]
-      const newTags = newTag
-        .match(/[^#\s]\S{0,}[^\s,]/g)
-        .map((tagText) => (
-          {
-            tagText: tagText,
-            id: tagText,
-            assignedTask: []
-          }
-        ));
-
-      accessTagDB("add", newTags);  // DB 변경
-      setNewTag("");  // 태그명 비우기
+    if (db === "Task") {
+      accessTaskDB(operation, task);
+    } else {
+      accessTagDB(operation, tag);
     }
-  }, [newTag]);
+  }, [currentWork]);
+
+
+  const addTags = useCallback((newTag) => {
+    // [{tagText: "태그1", id: "태그1", assignedTask: []}, { ... }, ...]
+    const newTags = newTag
+      .match(/[^#\s]\S{0,}[^\s,]/g)
+      .map((tagText) => (
+        {
+          tagText: tagText,
+          id: tagText,
+          assignedTask: []
+        }
+      ));
+
+    setCurrentWork({ action: "Tag/ADD", tag: newTags });
+    setTags((prev) => [...prev, ...newTags])
+  }, []);
 
   const deleteTag = (tagText) => {
     const tagObj = findObj(tags, tagText);
@@ -129,7 +131,24 @@ function App({ tasks, tagList }) {
     accessTagDB('delete', tagObj);
   };
 
-  const filterByTag = () => { };
+  const filterByTag = async (selection) => {
+    const { isSelected, tag } = selection;
+
+    if (!isSelected) {
+      setSelectedTags((prev) => {
+        return prev.filter((el) => el !== tag)
+      });
+    }
+    setSelectedTags((prev) => [...prev, tag]);
+    console.log(selectedTags);
+  };
+
+  useEffect(() => {
+    if (selectedTags.length) {
+      console.log(FilterByTagsDB(selectedTags));
+    }
+
+  }, [selectedTags])
 
 
   const taskCallbacks = {
@@ -140,8 +159,8 @@ function App({ tasks, tagList }) {
   };
 
   const tagCallbacks = {
-    onClick: deleteTag,
-    onChecked: filterByTag,
+    onDelete: deleteTag,
+    onFiltering: filterByTag,
   };
 
   const ongoingTasks = useMemo(() => {
@@ -157,8 +176,10 @@ function App({ tasks, tagList }) {
     <>
       <main className={side ? "sideshow" : ""}>
         <AddNewTask addTask={addTask} />
-        <AddNewTags tagText={newTag} callbacks={addTagCallbacks}>
-          {mappingComponent(tags, Tag, { makeChk: true })}
+        <AddNewTags addTags={addTags}>
+          <TagList callbacks={tagCallbacks}>
+            {mappingComponent(tags, Tag, { makeChk: true, callbacks: tagCallbacks })}
+          </TagList>
         </AddNewTags>
         <article className="todo_list">
           <TaskListSection sectionClass="ongoing">
