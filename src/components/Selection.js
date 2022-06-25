ae05b61eb9b9b2692c5350ebc084ab382067b937
ksheer506 <ksheer506@naver.com>
2022-06-25 22:12:49 +0900

    Selection 컴포넌트 추가(<select>)

---
+++ src/components/Selection.js
@@ -0,0 +1,15 @@
+
+const Selection = ({ title, children, onSelect }) => {
+
+  return (
+    <div className='select-tag'>
+      <span className='new-taskTags'>{title}</span>
+      <select onChange={onSelect}>
+        {children}
+      </select>
+    </div>
+  );
+}
+
+
+export default Selection;
