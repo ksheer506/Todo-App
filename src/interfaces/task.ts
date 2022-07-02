80017dbfcaddcdff48e73613fd322c1e02e4a763
ksheer506 <ksheer506@naver.com>
2022-07-02 23:00:00 +0900

    task 인터페이스 수정

--- src/interfaces/task.ts
+++ src/interfaces/task.ts
@@ -1,9 +1,15 @@
+import React from "react";
 import { taskDB } from "./db";
 
 export interface taskType extends taskDB {
   callbacks?: object; // FIXME:
 }
 
-export interface datePickerT extends taskType {
-  onChange: Function;
+export type datePickerT = Pick<taskType, "id" | "dueDate"> & {
+  onChange: (e: React.ChangeEvent, id: string) => void;
+};
+
+export interface taskList {
+  sectionClass: "ongoing" | "completed";
+  children: React.ReactNode;
 }
