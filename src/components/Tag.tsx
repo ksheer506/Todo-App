3caf92baf1c1ec192e5c51208271eb6c53b6978d
ksheer506 <ksheer506@naver.com>
2022-07-03 22:14:51 +0900

    minor update

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,41 +1,33 @@
-import React from 'react';
-import "./Tag.css"
+import React from "react";
+import "./Tag.css";
 const { useState, useEffect, useRef, useCallback } = React;
-
-
 
 function Tag(props) {
   const [selected, setSelected] = useState(false);
   const { tagText, makeChk, callbacks } = props;
-  const { onDelete, onFiltering } = callbacks;
+  const { onDelete, onFiltering } = callbacks || {};
 
   /* Tag 선택 토글 */
   const onTagSelected = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.checked) {
-      setSelected(true);
-      
-    } else {
-      setSelected(false);
-    }
-    onFiltering({isSelected: e.target.checked, tag: tagText})
+    setSelected(!!e.target.checked);
+    onFiltering({ isSelected: e.target.checked, tag: tagText });
   };
 
   return (
-    <label className={`tags ${selected ? "selected" : ""}`}>
-      {tagText}
-      {makeChk && <input type="checkbox" onChange={onTagSelected} />}
-    </label>  // 할일 필터링 용 체크박스
-  );
-};
-
-function TagList({ children }: {children: JSX.Element}) {
-
-  return (
-    <div className="tag-list">
-      {children}
+    <div className="tag-wrapper">
+      <label className={`tags ${selected ? "selected" : ""}`}>
+        {tagText}
+        {makeChk && <input type="checkbox" onChange={onTagSelected} />}
+      </label>
+      <button className="delete-tag" onClick={() => onDelete(tagText)}>
+        x
+      </button>
     </div>
-  )
+  );
+}
 
+function TagList({ children }: { children: JSX.Element }) {
+  return <div className="tag-list">{children}</div>;
 }
 
 export { Tag, TagList };
