be0582bcf91a9ca9d9d0e6214b1e3e262625c5c5
ksheer506 <ksheer506@naver.com>
2022-07-03 22:16:36 +0900

    Enter 입력 시 태그 추가될 수 있도록 기능 추가

--- src/components/AddNewItems.tsx
+++ src/components/AddNewItems.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './AddNewItems.css';
+import React from "react";
+import "./AddNewItems.css";
 const { useState, useEffect, useRef, useCallback } = React;
 
 type taskInput = "title" | "dueDate";
@@ -11,7 +11,7 @@ type taskInput = "title" | "dueDate";
 } */
 
 const AddNewTask = React.memo(function ({ addTask }) {
-  const [newTask, setNewTask] = useState({ title: "", dueDate: null })
+  const [newTask, setNewTask] = useState({ title: "", dueDate: null });
 
   const onEdit = (e: React.ChangeEvent<HTMLInputElement>, state: taskInput) => {
     setNewTask({ ...newTask, [state]: e.target.value });
@@ -19,12 +19,12 @@ const AddNewTask = React.memo(function ({ addTask }) {
   const onCreate = () => {
     setNewTask({ title: "", dueDate: null });
     addTask(newTask);
-  }
+  };
 
   return (
     <div>
       <header>
-        <h2 id='todo-title'>할 일 목록</h2>
+        <h2 id="todo-title">할 일 목록</h2>
         <div id="edit-title"></div>
         <div className="toggle-dark">
           <p>다크 모드</p>
@@ -33,33 +33,49 @@ const AddNewTask = React.memo(function ({ addTask }) {
         </div>
       </header>
       <div id="add-task">
-        <input type="text" placeholder="할 일을 입력해주세요." value={newTask.title} onChange={(e) => onEdit(e, "title")} />
+        <input
+          type="text"
+          placeholder="할 일을 입력해주세요."
+          value={newTask.title}
+          onChange={(e) => onEdit(e, "title")}
+        />
         <input type="date" onChange={(e) => onEdit(e, "dueDate")} />
         <input type="button" value="추가하기" onClick={onCreate} />
       </div>
     </div>
-
   );
 });
 
 const AddNewTags = React.memo(function (props) {
-  const [newTag, setNewTag] = useState("")
+  const [newTag, setNewTag] = useState("");
   const { addTags, children } = props;
 
-  const onNewTag = (e: React.MouseEvent<HTMLInputElement>) => {
+  const onNewTag = () => {
     addTags(newTag);
     setNewTag("");
   };
+  const onKeyUp = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      onNewTag();
+    }
+  }
 
   return (
     <div className="tag-box">
       <div className="tag-conf">
         <p>태그</p>
-        <input type="text" id="createTag" placeholder="ex. 태그1 태그2" value={newTag} onChange={(e) => setNewTag(e.target.value)} />
+        <input
+          type="text"
+          id="createTag"
+          placeholder="ex. 태그1 태그2"
+          value={newTag}
+          onKeyUp={onKeyUp}
+          onChange={(e) => setNewTag(e.target.value)}
+        />
         <input type="button" id="addTag" value="태그 추가" onClick={onNewTag} />
         <input type="button" id="deleteTag" value="태그 삭제" />
       </div>
-        {children}
+      {children}
     </div>
   );
 });
