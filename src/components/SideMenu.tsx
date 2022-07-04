8e822973b889dfa03a4b7dc88922143bbd4f275e
ksheer506 <ksheer506@naver.com>
2022-07-04 21:04:43 +0900

    text 수정 기능 추가

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,27 +1,45 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import { sidePanel } from "../interfaces/sidePanel";
+
 import "./SideMenu.css";
-
+import { Tag } from "./Tag";
 import Selection from "./Selection";
+import { editableField } from "../interfaces/task";
 
-const SideMenu = React.memo(function SideMenu({
-  id,
-  status,
-  title,
-  dueDate,
-  text,
-  tagDB,
-  callbacks,
-}: sidePanel) {
-  const { onClick, onSelect } = callbacks;
+
+
+const SideMenu = React.memo(function SideMenu({ id, status, title, dueDate, text, tagDB, callbacks }: sidePanel) {
+  const { onClick, onSelectTag, onEditTask } = callbacks;
+  const [onEdit, setOnEdit] = useState<{ field: editableField | null; newValue: string }>({ field: null, newValue: "" });
+  const editField = useRef(null);
 
   const tagList = tagDB.map((el) => (
     <option value={el.tagText} key={el.id}>
       {el.tagText}
     </option>
   ));
-  
+
+  /* 해당 Task가 가진 Tag */
+  const taskTag = tagDB.reduce((acc: Array<string>, cur) => {
+    const { tagText, assignedTask } = cur;
+    if (assignedTask.includes(id)) acc.push(tagText);
+
+    return acc;
+  }, []);
+
+  const onEditCompleted = () => {
+    onEditTask(id, onEdit);
+    setOnEdit({ ...onEdit, field: null });
+  };
+
+  useEffect(() => {
+    if (onEdit.field === "text") {
+      const textArea = editField.current! as HTMLTextAreaElement;
+      textArea.focus();
+      textArea.setSelectionRange(text.length, text.length);
+    }
+  }, [onEdit.field]);
 
   return (
     <aside className={status ? "sideshow" : ""}>
@@ -29,7 +47,7 @@ const SideMenu = React.memo(function SideMenu({
         <span
           className="close"
           onClick={() => {
-            onClick((prev) => ({ ...prev, status: false }));
+            onClick((prev: { status: boolean; id: string }) => ({ ...prev, status: false }));
           }}
         >
           x
@@ -38,19 +56,31 @@ const SideMenu = React.memo(function SideMenu({
       <h2>{title}</h2>
 
       <div className="dueDate">{dueDate}</div>
-      <div className="text">{text}</div>
+      {onEdit.field ? (
+        <div className="text" onBlur={onEditCompleted}>
+          <textarea
+            ref={editField}
+            value={onEdit.newValue || text}
+            onChange={(e) => setOnEdit({ ...onEdit, newValue: e.target.value })}
+          />
+        </div>
+      ) : (
+        <div className="text" onClick={() => setOnEdit({ ...onEdit, field: "text" })}>
+          {text}
+        </div>
+      )}
+
       {title ? (
-        <Selection
-          title="태그 추가"
-          onSelect={(e: React.ChangeEvent<HTMLSelectElement>) =>
-            onSelect(e, id)
-          }
-        >
+        <Selection title="태그 추가" onSelect={(e: React.ChangeEvent<HTMLSelectElement>) => onSelectTag(e, id)}>
           <option value=""></option>
           {tagList}
         </Selection>
       ) : null}
-      <div className="tag-list"></div>
+      <div className="tag-list">
+        {taskTag.map((tag) => (
+          <Tag tagText={tag} makeChk={false} key={tag} />
+        ))}
+      </div>
     </aside>
   );
 });
