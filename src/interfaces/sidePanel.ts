1321ce95a3abdb2ead709b1a1d5370f9ce659658
ksheer506 <ksheer506@naver.com>
2022-07-04 21:05:44 +0900

    사이드패널 callbacks 타입 설정

--- src/interfaces/sidePanel.ts
+++ src/interfaces/sidePanel.ts
@@ -1,7 +1,15 @@
+import { ChangeEvent } from "react";
 import { taskDB, tagDB } from "./db";
-import { taskType } from "./task";
+import { editableField } from "./task";
+
+interface callbacks {
+  onClick: (arg: { status: boolean; id: string }) => void;
+  onSelectTag: (e: ChangeEvent<HTMLSelectElement>, id: string) => void;
+  onEditTask: (taskId: string, { field, newValue }: { field: editableField | null; newValue: string | boolean }) => void
+}
 
 export interface sidePanel extends taskDB {
-  status: boolean;
-  tagDB: Array<tagDB>;
+  status: boolean,
+  tagDB: Array<tagDB>,
+  callbacks: callbacks
 }
