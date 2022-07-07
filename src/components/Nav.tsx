b6346840ecea1f706b0f75b39f0db66e09106d75
ksheer506 <ksheer506@naver.com>
2022-07-07 23:28:24 +0900

    Nav 컴포넌트 추가

---
+++ src/components/Nav.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Nav.css"
+
+const Nav = () => {
+
+  return (
+  <nav>
+    <input type="button" id="login" value="로그인" />
+    <input type="button" id="login" value="내보내기" />
+    <input type="button" id="login" value="가져오기" />
+  </nav>
+  )
+}
+
+export default Nav;
