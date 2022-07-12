97f92cee413894a7c88def551789a4987ca5c124
ksheer506 <ksheer506@naver.com>
2022-07-12 20:29:16 +0900

    DB 파일 내보내기 기능 테스트

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,22 @@
+import { promises as fs } from "fs";
 import React from "react";
+import { fetchAllData } from "../modules/db/fetching";
 
 import "./Nav.css"
 
 const Nav = () => {
 
+  const exportAllData = async () => {
+    const [task, tag] = await fetchAllData(["task", "tagList"]);
+    const formatted = {task,  tag}; 
+
+    /* await fs.writeFile("./test.json", JSON.stringify(formatted)) */
+  }
+
   return (
   <nav>
     <input type="button" id="login" value="로그인" />
-    <input type="button" id="login" value="내보내기" />
+    <input type="button" id="login" value="내보내기" onClick={exportAllData} />
     <input type="button" id="login" value="가져오기" />
   </nav>
   )
