5460b8c8d9395a7e552ccdf31b375d2108f6df9f
ksheer506 <ksheer506@naver.com>
2022-07-13 21:34:48 +0900

    불필요한 코드 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { TaskDB, TagDB } from "./interfaces/db";
-
-
-/* localStorage에 저장된 데이터(할일 제목, 다크모드 여부)를 불러오는 함수 */
-function loadLocalStorage() {
-  const userTitle = localStorage.getItem("title") || "할 일 목록";
-  const userDarkMode = localStorage.getItem("dark-mode");
-
-  if (userTitle) {
-    // Title 변경
-  }
-  /* if (userDarkMode === 'true') { // 유저 다크모드 적용
-    dom.darkToggler.setAttribute('checked', true);
-    darkModeSetter(true);
-  } */
-}
-
-/* 다크모드 토글 함수 */
-/* function darkModeSetter(checked = false) {
-  document.documentElement.setAttribute("dark-theme", checked);
-  localStorage.setItem("dark-mode", checked);
-} */
+import React from "react";
 
   const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
@@ -31,6 +9,6 @@ function loadLocalStorage() {
 
   root.render(
     <React.StrictMode>
-        <App />
+      <App />
     </React.StrictMode>
   );
